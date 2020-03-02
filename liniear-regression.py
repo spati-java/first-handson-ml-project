7da90cce68177042aa1b164a2bed155de3a3a431
Sourav Pati <sourav.pati@objectstream.com>
2020-03-02 10:15:12 -0600

    my first commit

---
+++ liniear-regression.py
@@ -0,0 +1,34 @@
+from random import randint 
+from sklearn.linear_model import LinearRegression
+
+TRAIN_SET_LIMIT = 1000
+TRAIN_SET_COUNT = 100
+
+TRAIN_INPUT = list();
+TRAIN_OUTPUT = list();
+
+for i in range(TRAIN_SET_COUNT):
+    a = randint(0,TRAIN_SET_LIMIT)
+    b = randint(0,TRAIN_SET_LIMIT)
+    c = randint(0, TRAIN_SET_LIMIT)
+    op = a + (2*b) + (3*c)
+    TRAIN_INPUT.append([a,b,c])
+    TRAIN_OUTPUT.append(op)
+ 
+predictor = LinearRegression(n_jobs= -1)
+predictor.fit(X = TRAIN_INPUT,y= TRAIN_OUTPUT)
+
+## TEST DATA
+
+X = [[10,20,30]]
+
+## The outcome should be 10 + 2* 20 + (3* 30) = 140
+
+X_TEST = [[20,20,30]]
+
+outcome = predictor.predict(X = X_TEST)
+coefficients = predictor.coef_
+
+print('Outcome : {}\nCoefficients : {}'.format(outcome, coefficients))
+
+
